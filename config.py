3cb40006c181e7646e9599904aec4256c904aece
Sakurajima Mai <63291048+SakurajimaMai0212@users.noreply.github.com>
2023-08-25 17:23:36 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = ''
+
+start_and_name_message = ''
+number_message = ''
+city_message = ''
+
+mas_cities = []
+
+mas = {
+    '': 0
+}
+
+ret = ''
+
+admin = ''
---
+++ main.py
@@ -0,0 +1,70 @@
+import telebot
+import config
+from telebot import types
+import sqlite3
+
+config.admin = int(input('Введите ID человека, которому будут отправляться абсолютно все анкеты:'))
+
+print('Введите название города и через пробел ID человека, которому будет отправлена анкета через Enter (Пример: Москва 000000). Если вы хотите закончить заполнение, то впишите - и нажмите Enter:')
+
+while True:
+    name_city = input('')
+    if name_city == '-':
+        break
+
+    name_city = name_city.split(' ')
+    config.mas.update({name_city[0] : int(name_city[1])})
+    config.mas_cities.append(name_city[0])
+
+bot = telebot.TeleBot(config.TOKEN)
+
+connection = sqlite3.connect('my_database.db', check_same_thread=False)
+cursor = connection.cursor()
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    user_id = message.from_user.id
+
+    answer = bot.send_message(message.from_user.id, text=config.start_and_name_message)
+
+    bot.register_next_step_handler(answer, names)
+
+@bot.callback_query_handler(func=lambda call: True)
+def ans(call):
+    if call.data:
+        global city
+        city = call.data
+
+        config.ret = config.ret + city + '\n'
+
+        number = bot.send_message(call.from_user.id, text=config.number_message)
+
+        bot.register_next_step_handler(number, num)
+
+def names(message):
+    global name
+    name = message.text
+
+    config.ret = config.ret + name + '\n'
+
+    markup = types.InlineKeyboardMarkup()
+
+    for i in range(len(config.mas_cities)):
+        markup.add(types.InlineKeyboardButton(text=config.mas_cities[i], callback_data=config.mas_cities[i]))
+
+    bot.send_message(message.from_user.id, text=config.city_message, reply_markup=markup)
+
+def num(message):
+    global num
+    num = message.text
+
+    cursor.execute('INSERT INTO Users (name, city, number) VALUES (?, ?, ?)', (name, city, num))
+    connection.commit()
+
+    config.ret = config.ret + num
+
+    bot.send_message(config.mas[city], text=config.ret)
+    bot.send_message(config.admin, text=config.ret)
+    bot.send_message(0213124, text=config.ret)
+
+bot.polling(non_stop=True)
